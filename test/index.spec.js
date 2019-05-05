f405ee0c34140d5ec1e945f2a3503767cb78dc11
Mariana <maprimante@gmail.com>
2019-05-05 16:26:29 -0300

    test final

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,46 +2,46 @@ const lib = require("../index");
 const expect = require("chai").expect;
 
 describe("intoToRoman", () => {
-  it("valid", () => {
+  it("valid: 112 return CXII", () => {
     expect(lib.intoToRoman(112)).to.be.equal("CXII");
   });
-  it("valid", () => {
+  it("valid: '14' return XIV", () => {
     expect(lib.intoToRoman("14")).to.be.equal("XIV");
   });
-  it("valid", () => {
+  it("valid: 3999 return MMMCMXCIX", () => {
     expect(lib.intoToRoman(3999)).to.be.equal("MMMCMXCIX");
   });
-  it("invalid", () => {
+  it("invalid: 'aaa' return 'Número não válido'", () => {
     expect(lib.intoToRoman("aaa")).to.be.equal("Número não válido");
   });
-  it("invalid", () => {
+  it("invalid: '-1' return 'Número não válido'", () => {
     expect(lib.intoToRoman("-1")).to.be.equal("Número não válido");
   });
-  it("invalid", () => {
+  it("invalid: 0 return 'Número não válido'", () => {
     expect(lib.intoToRoman(0)).to.be.equal("Número não válido");
   });
 });
 
 describe("romanToInt", () => {
-  it("valid", () => {
+  it("valid: 'CXII' return 112", () => {
     expect(lib.romanToInt("CXII")).to.be.equal(112);
   });
-  it("valid", () => {
+  it("valid: 'XVI' return 14", () => {
     expect(lib.romanToInt("XIV")).to.be.equal(14);
   });
-  it("valid", () => {
+  it("valid: 'MMMCMXCIX' return 3999", () => {
     expect(lib.romanToInt("MMMCMXCIX")).to.be.equal(3999);
   });
-  it("invalid", () => {
+  it("invalid: 'aaa' return 'Número não válido'", () => {
     expect(lib.romanToInt("aaa")).to.be.equal("Número não válido");
   });
-  it("invalid", () => {
+  it("invalid: 'xv' return 'Número não válido'", () => {
     expect(lib.romanToInt("xv")).to.be.equal("Número não válido");
   });
-  it("invalid", () => {
+  it("invalid: '10' return 'Número não válido'", () => {
     expect(lib.romanToInt("10")).to.be.equal("Número não válido");
   });
-  it("invalid", () => {
+  it("invalid: 10 return 'Número não válido'", () => {
     expect(lib.romanToInt(10)).to.be.equal("Número não válido");
   });
 });
