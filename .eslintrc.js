a60a12c06e6ddd31c22d5af72dc91efe5e3e1fa9
Mariana <maprimante@gmail.com>
2019-05-07 19:08:04 -0300

    mocha true

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "env": {"commonjs": true,"es6": true,"node": true},
+    "env": {"commonjs": true,"es6": true,"node": true, "mocha": true},
     "extends": "eslint:recommended",
     "globals": {"Atomics": "readonly","SharedArrayBuffer": "readonly"},
     "parserOptions": {"ecmaVersion": 2018},
